b698b728d69908979a395d9b0f215fb119f518b3
Bog2G <wegweg3eg@abv.bg>
2022-12-05 00:09:27 +0200

    db fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+
 
 
   resources :notes
   #get 'notebook/Home'
   root 'notes#index'
   get 'notebook/about'
-  resources :devise
   resources :notebook
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
